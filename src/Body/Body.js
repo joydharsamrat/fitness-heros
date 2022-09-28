edf7f5e6860b2ac7de86dd5525ce6fe0779dc4ed
joydharsamrat <joydharsamrat@gmail.com>
2022-09-28 22:55:57 +0600

    body created

---
+++ src/Body/Body.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Body.css'
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='activities-container'>
+                <h1>FitNess Heros</h1>
+            </div>
+            <div className='Current-activity'>
+                <h1>Samrat</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Body;
